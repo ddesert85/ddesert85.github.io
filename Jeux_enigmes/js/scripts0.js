d52493fffb63e45ad30bdd58b4afedd66e1bdcca
dorothée tartarin <dorothee.tartarin@hotmail.fr>
2023-09-11 17:37:13 +0200

    architecture HTML

---
+++ Jeux_enigmes/js/scripts0.js
@@ -0,0 +1,19 @@
+const reponse = document.getElementById("reponse");
+const submit = document.getElementById("submit")
+const pageSuivante = document.getElementById("pageSuivante")
+submit.addEventListener("click", EcouterReponse)
+
+function EcouterReponse(event) 
+{
+    event.preventDefault();
+    console.log(reponse.value);
+    console.log("J'ai clické")
+    if (reponse.value !== "samedi")
+    {
+        alert("Mauvaise réponse")
+    }
+
+    else {
+        pageSuivante.classList.remove("main__section--cachee")
+    }
+}
--- Jeux_enigmes/js/scripts1.js
+++ Jeux_enigmes/js/scripts1.js
@@ -1,6 +1,4 @@
 
-
-
 const reponse = document.getElementById("reponse");
 const submit = document.getElementById("submit")
 const pageSuivante = document.getElementById("pageSuivante")
@@ -11,12 +9,13 @@ function EcouterReponse(event)
     event.preventDefault();
     console.log(reponse.value);
     console.log("J'ai clické")
-    if (reponse.value !== "samedi")
+    if (reponse.value !== "8848")
     {
         alert("Mauvaise réponse")
     }
 
     else {
-        pageSuivante.classList.remove("pageSuivante")
+        pageSuivante.classList.remove("main__section--cachee")
     }
 }
+
