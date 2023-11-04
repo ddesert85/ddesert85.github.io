2a041aaa5b72d778bd6a0191c8c6bfbff6015aee
dorotheetartarin <dorothee.tartarin@hotmail.fr>
2023-11-04 18:08:32 +0100

    04.11.2023

--- Jeux_enigmes/js/scripts1.js
+++ Jeux_enigmes/js/scripts1.js
@@ -1,21 +1,21 @@
 
 const reponse = document.getElementById("reponse");
-const submit = document.getElementById("submit")
-const pageSuivante = document.getElementById("pageSuivante")
-submit.addEventListener("click", EcouterReponse)
+const submit = document.getElementById("submit");
+const pageSuivante = document.getElementById("pageSuivante");
+submit.addEventListener("click", EcouterReponse);
 
 function EcouterReponse(event) 
 {
-    event.preventDefault();
-    console.log(reponse.value);
-    console.log("J'ai clické")
-    if (reponse.value !== "8848")
-    {
-        alert("Mauvaise réponse")
-    }
+  event.preventDefault();
+  console.log(reponse.value);
+  console.log("J'ai clické");
+  if (reponse.value !== "8848")
+  {
+    alert("Mauvaise réponse");
+  }
 
-    else {
-        pageSuivante.classList.remove("main__section--cachee")
-    }
+  else {
+    pageSuivante.classList.remove("main__section--cachee");
+  }
 }
 
